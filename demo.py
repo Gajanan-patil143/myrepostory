215d4aa876776667b1b9d34d1e2769b83707a646
Gajanan-patil143 <gajananpatil8858@gmail.com>
2024-09-24 14:13:20 +0530

    installing java jdk

---
+++ demo.py
@@ -0,0 +1,14 @@
+#write a python program to print star pattern in python
+'''*
+**
+***
+****
+*****'''
+
+n=5
+for i in range(n):
+    for i in range(1,n):
+        print('*',end=" ")
+    
+
+    
